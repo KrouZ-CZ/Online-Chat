f2633daac2d5e9a59d6f153124f5f95f0bb0e003
KrouZ_CZ <64690903+KvantPro@users.noreply.github.com>
2022-05-01 21:10:05 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,41 +1,82 @@
 import socket
 import threading
+import json
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(('127.0.0.1', 2000))
+server.bind(('139.28.222.90', 2000))
 server.listen(4)
 
-users = {}
 rooms = {}
 
 class User:
 	def __init__(self, usr, adres):
 		self.user = usr
 		self.adres = adres
-		users[usr] = self.user.recv(1024).decode('utf-8')
-		self.passwd = self.user.recv(1024).decode('utf-8')
-		if rooms.get(self.passwd) == None:
-			rooms[self.passwd] = [self.user]
-		else:
-			rooms[self.passwd].append(self.user)
-		print(f"Connected: {users[self.user]}/{adres}")
-		for soct in rooms.get(self.passwd):
-			soct.send(f'User {users.get(self.user)} connected'.encode('utf-8'))
-		self.liss()
+		self.start()
+	def start(self):
+		self.n = 0
+		while True:
+			while True:
+				try:
+					self.data = json.loads(self.user.recv(1024).decode('utf-8').replace("'", '"'))
+				except:
+					return
+				if self.n == 1:
+					self.user.send('Disconnect'.encode('utf-8'))
+				if self.data[1] == 'Create':
+					if self.data[2] in rooms:
+						self.user.send('Error'.encode('utf-8'))
+					else:
+						self.user.send('Ok'.encode('utf-8'))
+						rooms[self.data[2]] = {}
+						rooms[self.data[2]]['pwd'] = self.data[3]
+						rooms[self.data[2]]['users'] = [self.user]
+						break
+				else:
+					if self.data[2] in rooms:
+						if rooms[self.data[2]]['pwd'] == self.data[3]:
+							rooms[self.data[2]]['users'].append(self.user)
+							self.user.send('Ok'.encode('utf-8'))
+							break
+						else:
+							self.user.send('Error'.encode('utf-8'))
+					else:
+						self.user.send('Error'.encode('utf-8'))
+			print(f'Connected: {self.data[0]}')
+			self.liss()
+			self.n = 1
 	def liss(self):
 		try:
 			while True:
-				self.data = self.user.recv(1024).decode('utf-8')
-				self.send(self.data)
+				self.msgd = self.user.recv(1024).decode('utf-8')
+				if self.msgd == 'Disconnect':
+					print(f"Disconnected: {self.data[0]}")
+					rooms[self.data[2]]['users'].remove(self.user)
+					if len(rooms[self.data[2]]['users']) == 0:
+						rooms.pop(self.data[2])
+					break
+				else:
+					self.send(self.msgd)
 		except:
-			print(f"Disconnected: {users.get(self.user)}")
-			rooms[self.passwd].remove(self.user)
-			users.pop(self.user)
-	def send(self, r):
-		for soct in rooms.get(self.passwd):
-			if soct == self.user: continue
-			a = [users.get(self.user), str(r)]
-			soct.send(str(a).encode('utf-8'))
+			print(f"Disconnected: {self.data[0]}")
+			rooms[self.data[2]]['users'].remove(self.user)
+			try:
+				if len(rooms[self.data[2]]['users']) == 0:
+					rooms.pop(self.data[2])
+			except:
+				pass
+	def send(self, msg):
+		for usr in rooms[self.data[2]]['users']:
+			try:
+				usr.send(str([self.data[0], msg]).encode('utf-8'))
+			except:
+				print(f"Disconnected: {usr}")
+				rooms[self.data[2]]['users'].remove(usr)
+				try:
+					if len(rooms[self.data[2]]['users']) == 0:
+						rooms.pop(self.data[2])
+				except:
+					pass
 def main():
 	while True:
 		user, adres = server.accept()
